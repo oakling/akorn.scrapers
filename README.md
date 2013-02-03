a6f8749e5b0f1a5221f33c5334d04bca2e980107
Michael Davis <m.r.davis@me.com>
2013-02-03 13:56:55 +0000

    Add the project framework

--- README.md
+++
@@ -1,2 +0,0 @@
-akorn.scrapers
-==============
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+version = open(os.path.join("akorn", "scrapers", "version.txt")).read().strip()
+
+setup(name='akorn_search',
+      version=version,
+      description="",
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Akorn',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['akorn', 'scrapers'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+)
