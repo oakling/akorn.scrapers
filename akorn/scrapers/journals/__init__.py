0953b9b9e6da95931afaf850d85d356edbbdcbff
Craig Loftus <craigloftus@gmail.com>
2013-07-25 20:45:21 +0100

    Adding scraper for rsta and rstb (Submitted by Victoria Watson)

--- akorn/scrapers/journals/__init__.py
+++ akorn/scrapers/journals/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['oup', 'scrape_AA', 'scrape_acs', 'scrape_arxiv', 'scrape_arxiv_api', 'scrape_atmos', 'scrape_cell', 'scrape_iop', 'scrape_jhu', 'scrape_meta_tags', 'scrape_mit', 'scrape_npg', 'scrape_nsub', 'scrape_oup', 'scrape_pnas', 'scrape_pr', 'scrape_prs', 'scrape_rsc', 'scrape_rup', 'scrape_wiley']
+__all__ = ['oup', 'rst', 'scrape_AA', 'scrape_acs', 'scrape_arxiv', 'scrape_arxiv_api', 'scrape_atmos', 'scrape_cell', 'scrape_iop', 'scrape_jhu', 'scrape_meta_tags', 'scrape_mit', 'scrape_npg', 'scrape_nsub', 'scrape_oup', 'scrape_pnas', 'scrape_pr', 'scrape_prs', 'scrape_rsc', 'scrape_rup', 'scrape_wiley', 'utils']
 # Don't modify the line above, or this line!
 import automodinit
 automodinit.automodinit(__name__, __file__, globals())
