adce0352ea72e8d7e0f0a88037fcac092bb6d743
Craig Loftus <craigloftus@gmail.com>
2013-08-18 18:10:35 +0100

    Fixing up the acs scraper

--- akorn/scrapers/journals/acs.py
+++ akorn/scrapers/journals/acs.py
@@ -1,13 +1,21 @@
 from akorn.scrapers.base import BaseScraper
 
-import dateutil
+from dateutil import parser
 import time
 
 class Scraper(BaseScraper):
+    # List of feeds that scraper is for
+    feeds = [
+                (
+                    "http://feeds.feedburner.com/acs/jacsat",
+                    "feedburner_origlink",
+                    {"minute":1, "hour": "15"},
+                )
+            ]
     # List of domains that scraper is for
     domains = ['pubs.acs.org']
     # Relative name of config file
-    config = 'acs.xml'
+    config = 'acs.json'
 
     def clean(self, data):
         data = super(Scraper, self).clean(data)
@@ -15,7 +23,7 @@ class Scraper(BaseScraper):
         date_str = data.get('date_published')
         if date_str:
             # Do fuzzy date parsing
-            date_obj = dateutil.parser.parse(date_str)
+            date_obj = parser.parse(date_str)
             # Convert date to timestamp
             data['date_published'] = time.mktime(date_obj.timetuple())
         return data
