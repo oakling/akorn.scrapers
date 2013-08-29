66f8ed8580f878b0acfa1491e6e0aea3cb207d87
Tim Green <timothy.green@gmail.com>
2013-08-29 20:13:22 +0100

    Add untested aps scraper

---
+++ akorn/scrapers/journals/aps.py
@@ -0,0 +1,45 @@
+from akorn.scrapers.base import BaseScraper
+
+from dateutil import parser
+import time
+
+class Scraper(BaseScraper):
+    # List of feeds that scraper is for
+    feeds = ["http://feeds.aps.org/rss/recent/prl.xml",
+             "http://feeds.aps.org/rss/recent/pra.xml",
+             "http://feeds.aps.org/rss/recent/prb.xml",
+             "http://feeds.aps.org/rss/recent/prc.xml",
+             "http://feeds.aps.org/rss/recent/prd.xml",
+             "http://feeds.aps.org/rss/recent/pre.xml",
+             "http://feeds.aps.org/rss/recent/prx.xml",]
+
+    # Where to find the URL in each feed item
+    feed_tag = 'link'
+
+    # List of domains that scraper is for
+    domains = ['pra.aps.org', 'prb.aps.org', 'prc.aps.org', 'prd.aps.org', 'pre.aps.org', 'prx.aps.org', 'prl.aps.org', 'prst-ab.aps.org', 'prst-per.aps.org', 'rmp.aps.org', 'prola.aps.org', 'link.aps.org',]
+
+    # Relative name of config file
+    config = 'aps.json'
+
+    def clean(self, data):
+        date_str = data['date_published']
+        
+        date_received = re.findall('Received\s+([0-9]+)\s+([A-Za-z]+)\s+([0-9]+)', date_str)
+        date_revised = re.findall('revised\s+([0-9]+)\s+([A-Za-z]+)\s+([0-9]+)', date_str)
+        date_published = re.findall('published\s+([0-9]+)\s+([A-Za-z]+)\s+([0-9]+)', date_str)
+
+        def make_datestamp(date_tuple):
+          year = int(date_tuple[2])
+          month = months[date_tuple[1]]
+          day = int(date_tuple[0])
+          return time.mktime(datetime.date(year, month, day).timetuple())
+
+        if date_received:
+          data['date_received'] = make_datestamp(date_received[0])
+        if date_revised:
+          data['date_revised'] = make_datestamp(date_revised[0])
+        if date_published:
+          data['date_published'] = make_datestamp(date_published[0])
+
+      return data
